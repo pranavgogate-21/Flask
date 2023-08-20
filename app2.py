5062a3db7bdd8d9f9b940cdac9eca6bf186cf49b
pranavgogate-21 <pranavgogate21@gmail.com>
2023-08-20 21:03:45 +0530

    added new files

--- app2.py
+++ app2.py
@@ -1,10 +1,13 @@
 from flask import Flask,redirect,url_for,render_template
 app=Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
+@app.route("/")
+def home():
     return render_template("index2.html",content=["virat","rohith","dhawan"])
 
+@app.route("/test")
+def test():
+    return render_template("new.html")
 
 if __name__ == "__main__":
     app.debug=True
